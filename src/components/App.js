4c29f56a43c104c4d4e3254887e436b242c922b6
Soleilxxx <alievsaley@gmail.com>
2023-12-07 14:12:07 +0400

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Logotipi from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleAllDelete() {
+    const confirmed = window.confirm("Are you sure?");
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div>
+      <Logotipi />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onDeleteAll={handleAllDelete}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
---
+++ src/components/Logo.js
@@ -0,0 +1,3 @@
+export default function Logo() {
+  return <h1>🛩 Saley Airlines 🎢</h1>;
+}
